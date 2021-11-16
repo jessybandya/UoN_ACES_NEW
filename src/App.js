71e1c75f640a82d37fca06bd2c8c52192fe5eedf
Jessy Bandya <jessy.bandya5@gmail.com>
2021-11-16 10:31:44 -0800

    New changes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Firstpage/pages/Home';
+import SignUp from './components/Firstpage/pages/SignupPage';
+import Pricing from './components/Firstpage/pages/PricingPage';
+import Footer from './components/Firstpage/components/Footer/Footer';
+import GlobalStyle from './components/Firstpage/globalStyles';
+import Navbar from './components/Firstpage/components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello</h1>
+		<Router>
+			<GlobalStyle />
+			<Navbar />
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/signup" exact component={SignUp} />
+				<Route path="/pricing" exact component={Pricing} />
+			</Switch>
+			<Footer />
+		</Router>
     </div>
   );
 }
---
+++ src/components/Hey.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hey() {
+    return (
+        <div>
+           Sawa 
+        </div>
+    )
+}
+
+export default Hey
