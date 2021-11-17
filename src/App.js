686f4f7ceae56803b425df8e03c8433dd0ad0cfb
Jessy Bandya <jessy.bandya5@gmail.com>
2021-11-17 13:28:00 -0800

    Changed background-color.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import SignUp from './components/Firstpage/pages/SignupPage';
 import Pricing from './components/Firstpage/pages/PricingPage';
 import Footer from './components/Firstpage/components/Footer/Footer';
 import GlobalStyle from './components/Firstpage/globalStyles';
-import Navbar from './components/Firstpage/components/Navbar/Navbar';
+import Navbar from './components/Grid/Navbar';
 import Grid from "./components/Grid"
 function App() {
   return (
     <div className="App">
 		<Router>
 			<GlobalStyle />
+			<Navbar />
 			<Switch>
-				<Route path="/" exact component={Grid} />
+				<Route path="/" exact component={Home} />
 				<Route path="/signup" exact component={SignUp} />
 				<Route path="/pricing" exact component={Pricing} />
 			</Switch>
