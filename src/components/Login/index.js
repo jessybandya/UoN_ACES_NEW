aec504c572d0d6090e83e4c878483c8227c20513
Jessy Bandya <jessy.bandya5@gmail.com>
2021-11-20 06:54:23 -0800

    Modified Login page.

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import './styles.css';
 import {auth} from './../firebase';
 import {useHistory} from 'react-router-dom';
+import {  toast } from 'react-toastify';
 
 
 function Login() {
@@ -21,55 +22,28 @@ function Login() {
         .catch((e) =>{
             if (
                 e.message ===
-                alert(e.message)
+                toast.error(e.message)
                 
                 
                 ) {
-                alert("The password is invalid or the user does not have a password");
-            } else if (
-                e.message ===
-                alert(e.message)
-            ) {
-                history.push("/register");
-                window.scrollTo({
-                    top: document.body.scrollHeight,
-                    left: 0,
-                    behavior: "smooth",
-                });
+                    toast.error("The password is invalid or the user does not have a password");
             }
         })
     }
     
     return (
         <>
-<div class="container bg-white pb-5">
-    <div class="row d-flex justify-content-start align-items-center mt-sm-5">
-        <div class="col-lg-5 col-10">
-            {/* <div id="circle"></div> */}
-            <div class="pb-5"> <img  className="img1" src="https://www.studentportal.news/wp-content/uploads/2018/08/UniversityOfNairobi.jpg" alt="" style={{maxHeight: "350px",opacity: "0.7",borderRadius: "50px"}}/> </div>
-        </div>
-        <div  class="col-lg-4 offset-lg-2 col-md-6 offset-md-3">
-            <div class="pt-4">
-                <h6><span class="fa fa-superpowers text-primary px-md-2"></span></h6>
-            </div>
-            <div style={{border: "1px solid #3f51b5",padding:10,borderRadius:10}} class="mt-3 mt-md-5">
-                <h5 style={{color: "#3f51b5"}}>Log in to your account</h5>
-                <hr/>
-                <form class="pt-4">
-                    <div class="d-flex flex-column pb-3"> <label style={{background: "",color:"#3f51b5",borderRadius: "10px"}} for="email">School Email</label> <input style={{color: ""}} onChange={(e) => setEmail(e.target.value)}  type="email" name="" id="emailId" class="border-bottom border-primary"/> </div>
-                    <div class="d-flex flex-column pb-3"> <label style={{background: "",color:"#3f51b5",borderRadius: "10px"}} for="password">Password</label> <input style={{color: ""}}  type="password" onChange={(e) => setPassword(e.target.value)}   name="passwrd" id="pwd" class="border-bottom border-primary"/> </div>
-                    <div class="d-flex jusity-content-end pb-4">
-                        <div class="ml-auto"> <a href="#" class="text-danger text-decoration-none"></a> </div>
-                    </div> <input type="button" style={{backgroundColor: "#3f51b5"}} onClick={login} type="submit" value="Log in" class="btn btn-primary btn-block mb-3"/> 
-                    <div class="register mt-5">
-                        <p>Don't have an account? <a style={{color: "#3f51b5"}} href="/register">Create an account</a></p>
-                    </div>
-                </form>
-                                </div>
-
-        </div>
+<div className="login">
+      <span className="loginTitle" style={{color: "#3f51b5"}}>UoN_ACES</span>
+      <form className="loginForm">
+        <label>School Email</label>
+        <input className="loginInput" type="text" onChange={(e) => setEmail(e.target.value)} placeholder="Enter your school email..." />
+        <label>Password</label>
+        <input className="loginInput" type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password..." />
+        <button onClick={login} className="loginButton">Login</button>
+      </form>
+      <button onClick={()=> toast.success("!oops, we're still working on this...")} className="loginRegisterButton">Forgot password?</button>
     </div>
-</div>
             
   </>      
     )
