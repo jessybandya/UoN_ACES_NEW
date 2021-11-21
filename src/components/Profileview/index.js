09fcc002bc6ef91307573d54ffcb0f59f4e1e3e2
Jessy Bandya <jessy.bandya5@gmail.com>
2021-11-21 04:02:53 -0800

    Added abbreviation of num.

--- src/components/Profileview/index.js
+++ src/components/Profileview/index.js
@@ -39,6 +39,40 @@ const useStyles = makeStyles((theme) => ({
 function Profileview() {
   const classes = useStyles();
 
+  function kFormatter(num) {
+    return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'K' : Math.sign(num)*Math.abs(num)
+}
+    
+function abbrNum(number, decPlaces) {
+  // 2 decimal places => 100, 3 => 1000, etc
+  decPlaces = Math.pow(10,decPlaces);
+
+  // Enumerate number abbreviations
+  var abbrev = [ "K", "M", "B", "T" ];
+
+  // Go through the array backwards, so we do the largest first
+  for (var i=abbrev.length-1; i>=0; i--) {
+
+      // Convert array index to "1000", "1000000", etc
+      var size = Math.pow(10,(i+1)*3);
+
+      // If the number is bigger or equal do the abbreviation
+      if(size <= number) {
+           // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+           // This gives us nice rounding to a particular decimal place.
+           number = Math.round(number*decPlaces/size)/decPlaces;
+
+           // Add the letter for the abbreviation
+           number += abbrev[i];
+
+           // We are done... stop
+           break;
+      }
+  }
+
+  return number;
+}
+
     return (
 <div class="container">
     <div class="main-body">
@@ -67,21 +101,21 @@ function Profileview() {
                       <div style={{margin: 5}}>                   
                       <div style={{color: "#3f51b5"}}><b>Follower(s)</b></div>
                       <div style={{fontWeight: "700",color: "#808080"}}>
-                      <NumberFormat value={188} displayType={'text'} thousandSeparator={true}  />
+                      {abbrNum(400000,3)}
                       </div>
                       </div>
 
                       <div style={{margin: 5}}>                   
                       <div style={{color: "#3f51b5"}}><b>Following</b></div>
                       <div style={{fontWeight: "700",color: "#808080"}}>
-                      <NumberFormat value={188} displayType={'text'} thousandSeparator={true}  />
+                      {abbrNum(1200000,3)}
                       </div>
                       </div>
 
                       <div style={{margin: 5}}>                   
                       <div style={{color: "#3f51b5"}}><b>Post(s)</b></div>
                       <div style={{fontWeight: "700",color: "#808080"}}>
-                    <NumberFormat value={188} displayType={'text'} thousandSeparator={true}  />
+                      {abbrNum(1500,3)}
                       </div>
                       </div>
                       </span>
