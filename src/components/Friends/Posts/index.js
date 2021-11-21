d0a404008c403e306e67218d8ecc731aef32294f
Jessy Bandya <jessy.bandya5@gmail.com>
2021-11-21 03:56:50 -0800

    Added Friends.

--- src/components/Friends/Posts/index.js
+++ src/components/Friends/Posts/index.js
@@ -19,6 +19,7 @@ import "../styles.css"
 import InsertCommentIcon from '@mui/icons-material/InsertComment';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import Backdrop from '@mui/material/Backdrop';
+import FormSelect from '../../forms/FormSelect';
 import {
   EmailShareButton,
   FacebookShareButton,
@@ -116,7 +117,7 @@ function Addpost() {
         <div class="d-flex align-items-center">
             <div class="image"> <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" class="rounded" width="155"/> </div>
             <div class="ml-3 w-100">
-                <h4 class="mb-0 mt-0">Alex Morrision</h4> <span>@jessybandya</span>
+                <h4 class="mb-0 mt-0">Jaby Boy</h4> <span>@jessybandya</span>
                 <div class="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
                     <div class="d-flex flex-column"> <span class="articles">Posts</span> <span class="number1">38</span> </div>
                     <div class="d-flex flex-column"> <span class="followers">Followers</span> <span class="number2">980</span> </div>
