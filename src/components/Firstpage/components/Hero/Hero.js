fb5133f85260465910300e3584237ac9c99d08af
Jessy Bandya <jessy.bandya5@gmail.com>
2021-11-25 08:24:09 -0800

    Modified first page.

--- src/components/Firstpage/components/Hero/Hero.js
+++ src/components/Firstpage/components/Hero/Hero.js
@@ -3,15 +3,22 @@ import { Link } from 'react-router-dom';
 import { Button, Container, MainHeading } from '../../globalStyles';
 import { HeroVideo, HeroSection, HeroText, ButtonWrapper, HeroButton } from './HeroStyles';
 import { auth } from "../../../firebase"
+import TypeWriter from "react-typewriter";
+
 const Hero = () => {
 	return (
 		<HeroSection>
-			<HeroVideo src="./assets/test.mp4" autoPlay muted control loop/>
+			<HeroVideo src="./assets/test.mp4" autoPlay muted  loop/>
 			<Container>
-				<MainHeading>UoN ACES <p>MOTIVATE || INSPIRE || INNOVATE</p></MainHeading>
-				<HeroText>
-					We provide the best civil engineering direction skills in and out of academic field
-				</HeroText>
+			<MainHeading><TypeWriter typing={0.5}>UoN ACES </TypeWriter><p><TypeWriter typing={0.5}>MOTIVATE || INSPIRE || INNOVATE</TypeWriter></p></MainHeading>
+				
+			<TypeWriter typing={1.0}>
+			<HeroText>
+					<i>We provide the best civil engineering direction skills in and out of academic field</i>
+				</HeroText>	
+			</TypeWriter>
+
+
 				<ButtonWrapper>
 					{!auth?.currentUser?.uid &&(
 					<Link to="/register">
