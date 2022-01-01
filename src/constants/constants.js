4ff6398d55b9d4747c3430f9a896319a31391e7e
Jessy Bandya <69303168+jessybandya@users.noreply.github.com>
2022-01-01 13:15:56 -0800

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const APP_ID = "199858cdfdf8f746"
-export const APP_REGION= "us"
-export const AUTH_KEY = "90b40269a786d34645b1017890b5b644c646ea9f"
+export const APP_ID = ""
+export const APP_REGION= ""
+export const AUTH_KEY = ""
